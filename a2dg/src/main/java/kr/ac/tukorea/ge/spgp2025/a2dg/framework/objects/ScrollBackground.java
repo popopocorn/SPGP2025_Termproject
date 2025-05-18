bd4c65843a06f91533c139d69a45d97102ac8ca2
popopocorn <popcorn375@naver.com>
2025-05-18 15:12:22 +0900

    배경 기초

---
+++ a2dg/src/main/java/kr/ac/tukorea/ge/spgp2025/a2dg/framework/objects/ScrollBackground.java
@@ -0,0 +1,25 @@
+package kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects;
+
+import android.graphics.Canvas;
+
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.Metrics
+public class ScrollBackground extends Sprite{
+    private int cw = (int)Metrics.width;
+    private int ch = (int)Metrics.height;
+    private int w = bitmap.getWidth();
+    private int h = bitmap.getHeight();
+
+    public ScrollBackground(int mipmapId) {
+        super(mipmapId);
+    }
+
+    @Override
+    public void update() {
+        super.update();
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+    }
+}
