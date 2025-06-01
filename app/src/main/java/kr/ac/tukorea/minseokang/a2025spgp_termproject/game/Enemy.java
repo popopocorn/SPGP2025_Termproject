65fdc83723fcc79333cb5703934eb15f5a6a52eb
popopocorn <popcorn375@naver.com>
2025-06-01 14:11:17 +0900

    적 생성

--- app/src/main/java/kr/ac/tukorea/minseokang/a2025spgp_termproject/game/Enemy.java
+++ app/src/main/java/kr/ac/tukorea/minseokang/a2025spgp_termproject/game/Enemy.java
@@ -6,16 +6,23 @@ import kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces.IBoxCollidable;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces.ILayerProvider;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces.IRecyclable;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.AnimSprite;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
 
 public class Enemy extends AnimSprite implements IRecyclable, IBoxCollidable, ILayerProvider<MainScene.Layer> {
 
-    protected float speed;
+    protected float speed = 320f;
     protected float ad;
     protected float hp;
     protected float exp;
     protected RectF collisionRect = new RectF();
     private float[] playerPosition;
 
+    public static Enemy get(int level, int index) {
+        return Scene.top().getRecyclable(Enemy.class).init(level, index);
+    }
+    private Enemy init(int level, int idx){
+        return this;
+    }
     public Enemy(int mipmapId, float fps) {
         super(mipmapId, fps);
     }
